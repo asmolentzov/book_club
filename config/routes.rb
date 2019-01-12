2f9b77c4663627b8d5a1dc7ca6df62c53e0c7b14
Anna Smolentzov <annasmolentzov@gmail.com>
2019-01-12 11:14:57 -0700

    Updates books and reviews in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books, only: [:index, :show, :new, :create, :destroy] do
-    resources :reviews, shallow: true, only: [:new, :create, :destroy]
-  end
+  # resources :books, only: [:index, :show, :new, :create, :destroy] do
+  #   resources :reviews, shallow: true, only: [:new, :create, :destroy]
+  # end
+  
+  get '/books', to: 'books#index'
+  post '/books', to: 'books#create'
+  get '/books/new', as: 'new_book', to: 'books#new' 
+  get '/books/:id', as: 'book', to: 'books#show'
+  delete '/books/:id', to: 'books#destroy'
+  
+  get '/books/:book_id/reviews/new', as: 'new_book_review', to: 'reviews#new'
+  post '/books/:book_id/reviews', as: 'book_reviews', to: 'reviews#create'
+  delete '/reviews/:id', as: 'review', to: 'reviews#destroy'
 
   root 'welcome#index'
   resources :users, only: [:show]
